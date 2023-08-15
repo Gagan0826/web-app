ba4838eab6ed392dda92196d40855a0dc769bc0b
Gagan <gaganbrgirish26@gmail.com>
2023-08-15 11:40:59 +0530

    Amazon secrets configured

--- config.py
+++ config.py
@@ -1,6 +1,18 @@
-customhost = "employee.csddeg9ckmuc.ap-south-1.rds.amazonaws.com"
-customuser = "admin"
-custompass = "Password!"
-customdb = "employee"
-custombucket = "addemployee"
+import boto3
+import json
+secret_name = "week-1-rds"
+region_name = "ap-south-1"
+client=boto3.client("secretsmanager", region_name=region_name)
+response=client.get_secret_value(SecretId="week-1-rds")
+secret_dict=json.loads(response['SecretString'])
+
+customhost = secret_dict['host']
+customuser = secret_dict['username']
+custompass = secret_dict['password']
+customdb = secret_dict['dbInstanceIdentifier']
+customregion = "ap-south-1"
+print("customhost",secret_dict['host'])
+print("customuser",secret_dict['username'])
+print("custompass",secret_dict['password'])
+print("customdb",secret_dict['dbInstanceIdentifier'])
 customregion = "ap-south-1"
